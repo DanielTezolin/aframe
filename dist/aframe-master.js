e9494ebab5018c986051e81be24efcd835774d23
DanielTezolin <tezolind@gmail.com>
2020-01-31 17:56:03 +0000

    Distribuição link inspector

--- dist/aframe-master.js
+++ dist/aframe-master.js
@@ -71168,126 +71168,123 @@ function getFog (data) {
 }
 
 },{"../../core/component":105,"../../lib/three":154,"../../utils/debug":175}],80:[function(_dereq_,module,exports){
-(function (process){
 /* global AFRAME */
 var AFRAME_INJECTED = _dereq_('../../constants').AFRAME_INJECTED;
 var pkg = _dereq_('../../../package');
 var registerComponent = _dereq_('../../core/component').registerComponent;
 var utils = _dereq_('../../utils/');
-
 /**
  * 0.4.2 to 0.4.x
  * Will need to update this when A-Frame goes to 1.x.x.
  */
-function getFuzzyPatchVersion (version) {
-  var split = version.split('.');
-  split[2] = 'x';
-  return split.join('.');
+function getFuzzyPatchVersion(version) {
+    var split = version.split('.');
+    split[2] = 'x';
+    return split.join('.');
 }
 
 var INSPECTOR_DEV_URL = 'https://aframe.io/aframe-inspector/dist/aframe-inspector.js';
 var INSPECTOR_RELEASE_URL = 'https://unpkg.com/aframe-inspector@' + getFuzzyPatchVersion(pkg.version) + '/dist/aframe-inspector.min.js';
-var INSPECTOR_URL = process.env.INSPECTOR_VERSION === 'dev' ? INSPECTOR_DEV_URL : INSPECTOR_RELEASE_URL;
+// var INSPECTOR_URL = process.env.INSPECTOR_VERSION === 'dev' ? INSPECTOR_DEV_URL : INSPECTOR_RELEASE_URL;
+var INSPECTOR_URL = 'https://scsinovacoes.com/controlscene/aframe-inspector.js'
 var LOADING_MESSAGE = 'Loading Inspector';
 var LOADING_ERROR_MESSAGE = 'Error loading Inspector';
 
 module.exports.Component = registerComponent('inspector', {
-  schema: {
-    url: {default: INSPECTOR_URL}
-  },
+    schema: {
+        url: { default: INSPECTOR_URL }
+    },
 
-  init: function () {
-    this.firstPlay = true;
-    this.onKeydown = this.onKeydown.bind(this);
-    this.onMessage = this.onMessage.bind(this);
-    this.initOverlay();
-    window.addEventListener('keydown', this.onKeydown);
-    window.addEventListener('message', this.onMessage);
-  },
+    init: function () {
+        this.firstPlay = true;
+        this.onKeydown = this.onKeydown.bind(this);
+        this.onMessage = this.onMessage.bind(this);
+        this.initOverlay();
+        window.addEventListener('keydown', this.onKeydown);
+        window.addEventListener('message', this.onMessage);
+    },
 
-  play: function () {
-    var urlParam;
-    if (!this.firstPlay) { return; }
-    urlParam = utils.getUrlParameter('inspector');
-    if (urlParam !== 'false' && !!urlParam) {
-      this.openInspector();
-      this.firstPlay = false;
+    play: function () {
+        var urlParam;
+        if (!this.firstPlay) { return; }
+        urlParam = utils.getUrlParameter('inspector');
+        if (urlParam !== 'false' && !!urlParam) {
+            this.openInspector();
+            this.firstPlay = false;
+        }
+    },
+
+    initOverlay: function () {
+        var dotsHTML = '<span class="dots"><span>.</span><span>.</span><span>.</span></span>';
+        this.loadingMessageEl = document.createElement('div');
+        this.loadingMessageEl.classList.add('a-inspector-loader');
+        this.loadingMessageEl.innerHTML = LOADING_MESSAGE + dotsHTML;
+    },
+
+    remove: function () {
+        this.removeEventListeners();
+    },
+
+    /**
+     * <ctrl> + <alt> + i keyboard shortcut.
+     */
+    onKeydown: function (evt) {
+        var shortcutPressed = evt.keyCode === 73 && evt.ctrlKey && evt.altKey;
+        if (!shortcutPressed) { return; }
+        this.openInspector();
+    },
+
+    showLoader: function () {
+        document.body.appendChild(this.loadingMessageEl);
+    },
+
+    hideLoader: function () {
+        document.body.removeChild(this.loadingMessageEl);
+    },
+
+    /**
+     * postMessage. aframe.io uses this to create a button on examples to open Inspector.
+     */
+    onMessage: function (evt) {
+        if (evt.data === 'INJECT_AFRAME_INSPECTOR') { this.openInspector(); }
+    },
+
+    openInspector: function (focusEl) {
+        var self = this;
+        var script;
+
+        // Already injected. Open.
+        if (AFRAME.INSPECTOR || AFRAME.inspectorInjected) {
+            AFRAME.INSPECTOR.open(focusEl);
+            return;
+        }
+
+        this.showLoader();
+
+        // Inject.
+        script = document.createElement('script');
+        script.src = this.data.url;
+        script.setAttribute('data-name', 'aframe-inspector');
+        script.setAttribute(AFRAME_INJECTED, '');
+        script.onload = function () {
+            AFRAME.INSPECTOR.open(focusEl);
+            self.hideLoader();
+            self.removeEventListeners();
+        };
+        script.onerror = function () {
+            self.loadingMessageEl.innerHTML = LOADING_ERROR_MESSAGE;
+        };
+        document.head.appendChild(script);
+        AFRAME.inspectorInjected = true;
+    },
+
+    removeEventListeners: function () {
+        window.removeEventListener('keydown', this.onKeydown);
+        window.removeEventListener('message', this.onMessage);
     }
-  },
-
-  initOverlay: function () {
-    var dotsHTML = '<span class="dots"><span>.</span><span>.</span><span>.</span></span>';
-    this.loadingMessageEl = document.createElement('div');
-    this.loadingMessageEl.classList.add('a-inspector-loader');
-    this.loadingMessageEl.innerHTML = LOADING_MESSAGE + dotsHTML;
-  },
-
-  remove: function () {
-    this.removeEventListeners();
-  },
-
-  /**
-   * <ctrl> + <alt> + i keyboard shortcut.
-   */
-  onKeydown: function (evt) {
-    var shortcutPressed = evt.keyCode === 73 && evt.ctrlKey && evt.altKey;
-    if (!shortcutPressed) { return; }
-    this.openInspector();
-  },
-
-  showLoader: function () {
-    document.body.appendChild(this.loadingMessageEl);
-  },
-
-  hideLoader: function () {
-    document.body.removeChild(this.loadingMessageEl);
-  },
-
-  /**
-   * postMessage. aframe.io uses this to create a button on examples to open Inspector.
-   */
-  onMessage: function (evt) {
-    if (evt.data === 'INJECT_AFRAME_INSPECTOR') { this.openInspector(); }
-  },
-
-  openInspector: function (focusEl) {
-    var self = this;
-    var script;
-
-    // Already injected. Open.
-    if (AFRAME.INSPECTOR || AFRAME.inspectorInjected) {
-      AFRAME.INSPECTOR.open(focusEl);
-      return;
-    }
-
-    this.showLoader();
-
-    // Inject.
-    script = document.createElement('script');
-    script.src = this.data.url;
-    script.setAttribute('data-name', 'aframe-inspector');
-    script.setAttribute(AFRAME_INJECTED, '');
-    script.onload = function () {
-      AFRAME.INSPECTOR.open(focusEl);
-      self.hideLoader();
-      self.removeEventListeners();
-    };
-    script.onerror = function () {
-      self.loadingMessageEl.innerHTML = LOADING_ERROR_MESSAGE;
-    };
-    document.head.appendChild(script);
-    AFRAME.inspectorInjected = true;
-  },
-
-  removeEventListeners: function () {
-    window.removeEventListener('keydown', this.onKeydown);
-    window.removeEventListener('message', this.onMessage);
-  }
 });
 
-}).call(this,_dereq_('_process'))
-
-},{"../../../package":51,"../../constants":97,"../../core/component":105,"../../utils/":179,"_process":5}],81:[function(_dereq_,module,exports){
+},{"../../../package":51,"../../constants":97,"../../core/component":105,"../../utils/":179}],81:[function(_dereq_,module,exports){
 var registerComponent = _dereq_('../../core/component').registerComponent;
 var shouldCaptureKeyEvent = _dereq_('../../utils/').shouldCaptureKeyEvent;
 
@@ -80410,7 +80407,7 @@ _dereq_('./core/a-mixin');
 _dereq_('./extras/components/');
 _dereq_('./extras/primitives/');
 
-console.log('A-Frame Version: 1.0.3 (Date 2020-01-31, Commit #b3f48b66)');
+console.log('A-Frame Version: 1.0.3 (Date 2020-01-31, Commit #e7141cfd)');
 console.log('three Version (https://github.com/supermedium/three.js):',
     pkg.dependencies['super-three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
